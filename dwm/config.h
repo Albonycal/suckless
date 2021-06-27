6628210e1cb96eb47bcbec9d039fac5363e87ccc
Albony Cal <albonycal@gmail.com>
2021-06-27 13:21:09 +0530

    colors removed

--- dwm/config.h
+++ dwm/config.h
@@ -12,16 +12,6 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack Nerd Font:size=9" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#0F111A";
-static const char col_gray2[]       = "#E5E9F0";
-static const char col_gray3[]       = "#E5E9F0";
-static const char col_gray4[]       = "#E5E9F0";
-static const char col_cyan[]        = "#3A575C";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
 
 typedef struct {
 	const char *name;
